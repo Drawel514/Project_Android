b8fdd3ef6913f30fe7d950411573c591f4f7f085
Daniel Rojas <darojas061@gmail.com>
2023-08-01 15:06:15 -0500

    Primer commit

--- app/src/main/java/com/DanielRojas/ADSI/login.java
+++ app/src/main/java/com/DanielRojas/ADSI/login.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    //Atributos
-
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login2);
     }
 }
